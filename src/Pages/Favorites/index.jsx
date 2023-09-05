3312fe7370d43fa2b71b7903c7ead7e1550ffb4d
samrjj <45480413+samrjj32@users.noreply.github.com>
2023-09-05 14:46:53 +0530

    Update index.jsx

--- src/Pages/Favorites/index.jsx
+++ src/Pages/Favorites/index.jsx
@@ -6,16 +6,13 @@ import "./favorites.css";
 const Favorites = () => {
   const { data } = useSelector((state) => state.favorite);
 
-  const handleClickLike = () => {
-    console.log("hey");
-  };
 
   return (
     <div className="container">
       {data?.map((item, i) => {
         return (
           <div key={i}>
-            <ImageContainer data={item} onClick={handleClickLike} />
+            <ImageContainer data={item}  />
           </div>
         );
       })}
